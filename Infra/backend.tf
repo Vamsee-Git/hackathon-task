b3c30e328f1c0592e07995aa5a73c37fd364678a
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-05-15 11:00:22 +0530

    Update backend.tf

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -1,7 +1,7 @@
 terraform{
   backend "s3" {
     bucket         = "terraform-state-backend-vamsee"
-    key            = "terraform/dynamodb"
+    key            = "terraform/hackathon-task"
     region         = "us-west-1"
     encrypt        = true
     ##dynamodb_table = "Dynamodb-lock"
