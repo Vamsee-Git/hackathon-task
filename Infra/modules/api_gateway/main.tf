7a560edf3e89657314d742beaff6ef690804158c
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-05-15 12:59:20 +0530

    Update main.tf

--- Infra/modules/api_gateway/main.tf
+++ Infra/modules/api_gateway/main.tf
@@ -45,8 +45,7 @@ resource "aws_lambda_permission" "apigw_permission" {
 variable "lambda_function_arn" {
   description = "The ARN of the Lambda function"
   type        = string
-
-
+}
 variable "lambda_function_name" {
   description = "The name of the Lambda function"
   type        = string
